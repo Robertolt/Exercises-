a9a16ea44123581702071ab93120022eab7ff038
Robertolt <rl.beto.lorenzoni@gmail.com>
2020-11-04 19:55:29 -0300

    Exercicios

---
+++ .venv/Higher_number.py
@@ -0,0 +1,12 @@
+#The progam should show the higest number between two numbers
+#O programa deve mostrar o maior numero
+
+"Fit in 2 numbers"
+
+num1 = float(input('put number 1: '))
+num2 = float(input('put number 2: '))
+
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
