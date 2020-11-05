f5d09874c2729c52d94a9eab5a480775fc2144f7
Robertolt <rl.beto.lorenzoni@gmail.com>
2020-11-05 13:29:25 -0300

    Exercicios

---
+++ .venv/Negative_or_Positive.py
@@ -0,0 +1,9 @@
+"Develop a program that asks a number and return negative or positive acording to it"
+"Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo."
+
+number = float(input("Type in the number: "))
+if number>0:
+    print('Positive')
+else:
+    print('Negative')
+    
