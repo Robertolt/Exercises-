eedb0938cb13fcd97724c92f0bb27a313254e1f2
Robertolt <rl.beto.lorenzoni@gmail.com>
2020-11-05 13:55:51 -0300

    Exercicios

---
+++ .venv/Male_Female.py
@@ -0,0 +1,17 @@
+"""Develop a program witch verifies if the written letter is F ou M. According to the letter, the program should return
+(F)emale, (M)ale or Invalid"""
+
+"""Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - 
+Masculino, Sexo Inválido."""
+
+gender = input('Type on a letter for the gender: ')
+if gender == 'M':
+    print('MALE')
+elif gender == 'm':
+    print('MALE')
+elif gender == 'F':
+    print('Female')
+elif gender == 'f':
+    print('Female')
+else:
+    print('Invalid')
