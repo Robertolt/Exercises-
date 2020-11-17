8f488e082f3c9f4086f814722e778b91ae1e48e8
Robertolt <rl.beto.lorenzoni@gmail.com>
2020-11-17 16:34:05 -0300

    Exercicios

---
+++ .venv/int_float.py
@@ -0,0 +1,6 @@
+number = float(input('Input number: '))
+
+if number == round(number):
+    print('integer number')
+else:
+    print('decimal number')
---
+++ .venv/operations_numeric.py
@@ -0,0 +1,19 @@
+number1 = float(input('Type in the first number: '))
+number1 = float(input('Type in the second number: '))
+operation = input('Type the wished operation: 1 for +\n2 for - ')
+
+if (operation == 1):
+    number = (number1 + number2)
+elif (operation == 2):
+    number = (number1 - number2)
+elif (operation == 3):
+    number = (number1 * number2)
+elif (operation == 4):
+    number = (number1 ** number2)
+elif (operation == /):
+    number = (number1 / number2)
+elif (operation == //):
+    number = (number1 // number2)
+elif operation == %:
+    number = (number1 % number2)
+
