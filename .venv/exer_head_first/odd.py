111f62169cd9ef3939c61930aaf2df6046be5cda
robertolt <rl.beto.lorenzoni@gmail.com>
2020-12-08 16:54:14 -0300

    aow macho vei

--- .venv/exer_head_first/odd.py
+++ .venv/exer_head_first/odd.py
@@ -1,15 +1,18 @@
 from datetime import datetime
+from time import sleep
+from random import randint
 
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33,
         35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
 for i in range(5):
+    sleep(randint(1,5))
     right_this_minute = datetime.today().minute
-
     if right_this_minute in odds:
         print("This minute seems little odd.")
     else:
         print("not an odd minute.")
+
 
 import datetime
 print(datetime.date.today().day)
