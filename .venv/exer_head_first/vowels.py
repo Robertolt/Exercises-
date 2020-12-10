6e168f13ff835d99f2ae34f8dd8199506d5b86fe
robertolt <rl.beto.lorenzoni@gmail.com>
2020-12-10 18:06:31 -0300

    aow macho vei

---
+++ .venv/exer_head_first/vowels.py
@@ -0,0 +1,9 @@
+word = input('Input a word:' )
+
+vowels = ['a','e','i','o','u']
+
+
+
+for letters in word:
+    if letters in vowels:
+        print(letters)
---
+++ .venv/exer_head_first/vowels2.py
@@ -0,0 +1,12 @@
+word = input('Input a word:' )
+vowels = ['a','e','i','o','u']
+found = []
+
+
+
+for letters in word:
+    if letters in vowels:
+        if letters not in found:
+            found.append(letters)
+for letters in found:
+    print(letters)
