0790d9e5e41c8aa996f5928fbb99497e9bce4bb6
robertolt <rl.beto.lorenzoni@gmail.com>
2021-11-03 21:24:47 -0300

    feito os 4 primeiros exercicios de sequencias de repeticao

---
+++ .venv/.tt/estrutura_de_repeticao/exer15.py
@@ -0,0 +1,19 @@
+"""A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,...
+Faça um programa capaz de gerar a série até o n−ésimo termo."""
+
+n = int(input('input the series n number: '))
+ultimo = 1
+penultimo = 1
+
+if n == 1 or n == 2:
+    print('n=1')
+else:
+    for _ in range(2, n):
+        proximo = ultimo + penultimo
+        penultimo = ultimo
+        ultimo = proximo
+    print(proximo)
+
+
+
+
