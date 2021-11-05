1cfbbeff14444344cc83a9c730bd365417c1b048
robertolt <rl.beto.lorenzoni@gmail.com>
2021-11-05 16:07:58 -0300

    feito os 4 primeiros exercicios de sequencias de repeticao

--- .venv/.tt/estrutura_de_repeticao/exer18.py
+++ .venv/.tt/estrutura_de_repeticao/exer18.py
@@ -3,7 +3,7 @@
 lista = []
 N_numeros = int(input('coloque a quantidade de números desejada: '))
 
-for _ in range(N_numeros):
+for _ in range(N_numeros): # 0 1 2 3 4
     lista.append(int(input('digite os números desejados: ')))
 
 print('o número máximo é', max(lista), 'o menor número é', min(lista), 'a soma é', sum(lista))
